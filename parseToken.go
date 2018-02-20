510e3358f25aafc318fc7b38b04f86966073987e
kostya <konstantin.nizhinskij@privatbank.ua>
2018-02-20 22:02:17 +0200

    add dev handler

--- parseToken.go
+++ parseToken.go
@@ -59,7 +59,6 @@ func GinAuthHandler() gin.HandlerFunc {
 		tokenString := c.GetHeader(header_key)
 		if len(tokenString) >= 1 {
 			tokenString = strings.TrimPrefix(tokenString, "Bearer ")
-			log.Println(tokenString)
 			user := GetUser(tokenString)
 			user.Locale = c.GetHeader("locale")
 			c.Set("User", user)
