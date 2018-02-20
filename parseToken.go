d08886457cb5d2735a24620a6b56b3b83b9d27b8
kostya <konstantin.nizhinskij@privatbank.ua>
2018-02-20 21:46:02 +0200

    add dev handler

--- parseToken.go
+++ parseToken.go
@@ -26,7 +26,7 @@ func GetUser(tokenString string) *User{
 	if user, ok := token.Claims.(*User); ok && token.Valid {
 		user.JWT_Key = tokenString
 
-		return user
+		return User(user)
 	}
 	panic(errors.New("Error token user"))
 
@@ -49,12 +49,12 @@ func GinAuthHandlerDev() gin.HandlerFunc {
 
 func GinAuthHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		defer func () {
+	/*	defer func () {
 			if r := recover(); r != nil {
 				c.JSON(http.StatusBadRequest, gin.H{})
 				c.Abort()
 			}
-		}()
+		}()*/
 		tokenString := c.GetHeader(header_key)
 		if len(tokenString) >= 1 {
 			tokenString = strings.TrimPrefix(tokenString, "Bearer ")
